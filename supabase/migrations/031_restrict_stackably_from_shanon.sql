108b89e1e2457ec0d5e4b1cdd1d3dcc7d484bee2
Doug Williams <bizadvisordoug@gmail.com>
2026-07-16 15:09:48 -0500

    Fix column name in Shanon access restriction policy

--- supabase/migrations/031_restrict_stackably_from_shanon.sql
+++ supabase/migrations/031_restrict_stackably_from_shanon.sql
@@ -1,10 +1,10 @@
 -- Restrict Shanon from seeing Stackably leads
--- This policy prevents Shanon Boos from viewing any leads with Stackably as the suggested POS system
+-- This policy prevents Shanon Boos from viewing any leads with Stackably as the POS system
 create policy "Shanon cannot see Stackably leads" on public.leads
   for select using (
     -- Allow if user is Shanon and lead is NOT Stackably
     case
-      when auth.uid() = 'e128723f-8315-43f1-a67b-c5b9fd309ce1'::uuid then suggested_pos_system != 'Stackably'
+      when auth.uid() = 'e128723f-8315-43f1-a67b-c5b9fd309ce1'::uuid then pos_system != 'Stackably'
       else true
     end
   );
